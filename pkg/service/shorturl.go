0f62021a8cebd2db53198732e234329831463c42
dodoiyp <dodoiyp@gmail.com>
2021-07-08 16:38:13 +0800

    add time constant

--- pkg/service/shorturl.go
+++ pkg/service/shorturl.go
@@ -10,8 +10,6 @@ import (
 	"gorm.io/gorm"
 )
 
-const timeFormat = "2006-01-02 15:04:05"
-
 func NewShortService(c *gin.Context) ShortUrlSevice {
 	return &Service{
 		db: global.Mysql,
@@ -29,10 +27,9 @@ func (s *Service) CreateShortUrl(u *models.Url) error {
 	return nil
 }
 
-//TODO : add where expiretime filter
 func (s *Service) GetUrl(shortUrl string) (*models.Url, error) {
 	u := &models.Url{}
-	result := s.db.Where("short_url = ? AND expire_at> ?", shortUrl, time.Now().Format(timeFormat)).First(&u)
+	result := s.db.Where("short_url = ? AND expire_at> ?", shortUrl, time.Now().Format(global.SecLocalTimeFormat)).First(&u)
 	if result.Error != nil {
 		return nil, result.Error
 	}
